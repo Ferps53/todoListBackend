0c39edcb4d0eda8b5b21022d633bbca4c40d6546
Felipe Brostolin <felipe.brostolin53@gmail.com>
2023-12-27 19:47:02 -0300

    Add: Shutdown no ExecutorService

--- src/main/java/com/ferps/todo/schedules/NotificacaoSchedule.java
+++ src/main/java/com/ferps/todo/schedules/NotificacaoSchedule.java
@@ -50,6 +50,9 @@ public class NotificacaoSchedule {
             while(!processo.isDone()){
                 processo.get();
             }
+            if(executorService.isTerminated()){
+                executorService.shutdown();
+            }
         }
 
         if(!messageList.isEmpty()){
